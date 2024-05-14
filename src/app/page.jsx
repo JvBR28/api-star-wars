e7c4e40a3e8a6556b761d18994d483dafc11d58f
JvBR <joaovictor.brodr@gmail.com>
2024-05-14 15:02:12 -0300

    <FEAT>[Pagination]: Adding simple pagination

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,21 +2,32 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 
-export default function Home() {
+const Home = () => {
   const [people, setPeople] = useState([]);
+  const [nextPage, setNextPage] = useState('');
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = async (url) => {
       try {
-        const res = await fetch('https://swapi.dev/api/people');
+        const res = await fetch(url || 'https://swapi.dev/api/people');
         const data = await res.json();
         setPeople(data.results);
+        setNextPage(data.next);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
     fetchData();
   }, []);
+
+  const fetchNextPage = async () => {
+    if (nextPage) {
+      const res = await fetch(nextPage);
+      const data = await res.json();
+      setPeople([...people, ...data.results]);
+      setNextPage(data.next);
+    }
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
@@ -51,7 +62,7 @@ export default function Home() {
         </ul>
       </nav>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <ul className="grid grid-cols-4 gap-4 item-center">
+      <ul className="grid grid-cols-4 gap-4 item-center">
           {people.map((person, index) => {
             const id = person.url.split('/').filter(Boolean).pop();
             return (
@@ -69,6 +80,15 @@ export default function Home() {
           })}
         </ul>
       </div>
+      {nextPage && (
+        <div className="flex justify-center mt-4">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={fetchNextPage}>
+            Carregar mais
+          </button>
+        </div>
+      )}
     </main>
   );
-}
+};
+
+export default Home;
